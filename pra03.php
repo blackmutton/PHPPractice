fbb76b5d6d2cdfc8aed76d2068a42e68041adaf9
yt Chen <yutongc30@gmail.com>
2024-04-15 15:27:51 +0800

    完成正三角形

--- pra03.php
+++ pra03.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>畫星星</title>
+    <style>
+        *{
+            font-family:'Courier New', 'Courier',monospace;
+        }
+    </style>
 </head>
 <body>
 <h2>直角三角形</h2>
@@ -15,14 +20,42 @@
     }
     echo "<br>";
 } */
-
-for($i=1;$i<=5;$i++){
-    
+$stars=5;
+for($i=1;$i<=$stars;$i++){
+    //控制幾行
     for($j=1;$j<=$i;$j++){
+    //控制一行有多少顆星星
     echo "*";
 }
     echo "<br>";
 }
+?>
+
+<h2>倒直角三角形</h2>
+<?php
+for($i=$stars;$i>0;$i--){
+    //外層最為關鍵，因此先調整外層
+    for($j=1;$j<=$i;$j++){
+        echo "*";
+    }
+    echo "<br>";
+}
+?>
+
+<h2>正三角形</h2>
+<?php
+for($i=0;$i<$stars;$i++){
+    for($k=0;$k<$stars-$i;$k++){
+        echo "&nbsp;";
+        //unbreak space
+    }
+    for($j=0;$j<$i*2+1;$j++){
+        
+        echo "*";
+    }
+    echo "<br>";
+}
+
 ?>
 </body>
 </html>
