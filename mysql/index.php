02b5b64bf6e30fdaf1d3eec78d084cc8982b579f
yt Chen <yutongc30@gmail.com>
2024-06-30 19:01:16 +0800

    使用PHP在網頁上顯示資料庫的資料

--- mysql/index.php
+++ mysql/index.php
@@ -1,4 +1,53 @@
+<style>
+    table {
+        border-collapse: collapse;
+
+    }
+
+    td {
+        padding: 5px 10px;
+        border: 1px solid #999;
+    }
+</style>
 <?php
 $dsn="mysql:host=localhost;charset=utf8;dbname=school";
 $pdo=new PDO($dsn,'root','');
+$sql="select * from `student` where `id` <30";
+$db=$pdo->query($sql);
+$rows=$db->fetchAll()
+/* echo "<pre>";
+print_r($rows);
+echo "</pre>";
+ */
+echo "<table>";
+echo "<tr>";
+echo "<td>id</td>";
+echo "<td>學號</td>";
+echo "<td>姓名</td>";
+echo "<td>生日</td>";
+echo "<td>身份證號</td>";
+echo "<td>地址</td>";
+echo "<td>父母</td>";
+echo "<td>電話</td>";
+echo "<td>科系</td>";
+echo "<td>畢業學校</td>";
+echo "<td>畢業狀態</td>";
+echo "</tr>";
+foreach ($rows as $row) {
+    echo "<tr>";
+    echo "<td>{$row['id']}</td>";
+    echo "<td>{$row['school_num']}</td>";
+    echo "<td>{$row['name']}</td>";
+    echo "<td>{$row['birthday']}</td>";
+    echo "<td>{$row['uni_id']}</td>";
+    echo "<td>{$row['addr']}</td>";
+    echo "<td>{$row['parents']}</td>";
+    echo "<td>{$row['tel']}</td>";
+    echo "<td>{$row['dept']}</td>";
+    echo "<td>{$row['graduate_at']}</td>";
+    echo "<td>{$row['status_code']}</td>";
+    echo "</tr>";
+}
+
+echo "</table>";
 ?>
